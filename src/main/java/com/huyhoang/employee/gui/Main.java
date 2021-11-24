7e305ec30daf2a25a59e23d807592d4315edd171
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2021-11-24 16:13:51 +0700

    Sua gd

--- src/main/java/com/huyhoang/employee/gui/Main.java
+++ src/main/java/com/huyhoang/employee/gui/Main.java
@@ -1,37 +1,20 @@
 package com.huyhoang.employee.gui;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
-import com.huyhoang.employee.gui.component.Content;
-import com.huyhoang.employee.gui.component.Header;
-import com.huyhoang.employee.gui.component.Menu;
-import com.huyhoang.employee.gui.component.TabLayout;
-import com.huyhoang.employee.gui.form.Form2;
-import com.huyhoang.employee.gui.form.TourForm;
-import com.huyhoang.swing.event.EventMenuSelected;
 import com.huyhoang.swing.panel.ComponentResizer;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.text.DecimalFormat;
-import javax.swing.JLayeredPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import net.miginfocom.swing.MigLayout;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 public class Main extends javax.swing.JFrame {
 
-    private boolean show = true;
-    private MigLayout layout;
-    private Menu menu;
-    private Header header;
-    private Content content;
-    private TabLayout tab;
-    private final DecimalFormat df = new DecimalFormat("##0.###");
+    private boolean show;
+    private Animator start;
 
     public Main() {
         initComponents();
@@ -40,6 +23,10 @@ public class Main extends javax.swing.JFrame {
     }
 
     private void buildDisplay() {
+        start();
+    }
+
+    private void start() {
         setBackground(new Color(0, 0, 0, 0));
         setOpacity(0);
         TimingTarget target = new TimingTargetAdapter() {
@@ -59,40 +46,26 @@ public class Main extends javax.swing.JFrame {
                 }
             }
         };
-        Animator animator = new Animator(300, target);
-        animator.setResolution(0);
-        animator.setAcceleration(0.5f);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-                if (!animator.isRunning()) {
-                    animator.start();
-                }
-            }
-        });
-        layout = new MigLayout("fill, insets 0", "195[fill]0", "0[fill]0");
-        bg.setLayout(layout);
-        menu = new Menu();
-        menu.addEventMenuSelected(new EventMenuSelected() {
-            @Override
-            public void menuSelected(int index) {
-                if (index == 0) {
-                    content.showForm(new TourForm());
-                } else if (index == 1) {
-                    content.showForm(new Form2());
-                }
-            }
-        });
+        start = new Animator(300, target);
+        start.setResolution(0);
+        start.setAcceleration(0.5f);
+    }
 
-        header = new Header();
-        header.moving(this);
-        bg.setLayer(menu, JLayeredPane.POPUP_LAYER);
-        bg.add(menu, "pos 0al 0al 200 100%");
-        bg.add(header, "h 70!, wrap");
+    @Override
+    public void setVisible(boolean b) {
+        super.setVisible(b);
+        show = b;
+        if (show) {
+            start.start();
+        }
+    }
 
-        content = new Content();
-        content.showForm(new TourForm());
-        bg.add(content);
+    private void close() {
+        if (start.isRunning()) {
+            start.stop();
+        }
+        show = false;
+        start.start();
     }
 
     private void resized() {
@@ -107,15 +80,45 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         bg = new com.huyhoang.swing.panel.LayerPaneShadow();
+        menu1 = new com.huyhoang.employee.gui.component.Menu();
+        header1 = new com.huyhoang.employee.gui.component.Header();
+        content1 = new com.huyhoang.employee.gui.component.Content();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setName(""); // NOI18N
         setUndecorated(true);
 
         bg.setBackground(new java.awt.Color(255, 255, 255));
-        bg.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        bg.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
         bg.setShadowOpacity(0.3F);
-        bg.setShadowSize(20);
+        bg.setShadowSize(10);
+
+        bg.setLayer(menu1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        bg.setLayer(header1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        bg.setLayer(content1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgLayout.createSequentialGroup()
+                .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(header1, javax.swing.GroupLayout.DEFAULT_SIZE, 1226, Short.MAX_VALUE)
+                    .addComponent(content1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgLayout.createSequentialGroup()
+                .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(bgLayout.createSequentialGroup()
+                .addComponent(header1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(content1, javax.swing.GroupLayout.DEFAULT_SIZE, 789, Short.MAX_VALUE))
+        );
+
         getContentPane().add(bg, java.awt.BorderLayout.CENTER);
 
         setSize(new java.awt.Dimension(1460, 881));
@@ -149,5 +152,8 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.huyhoang.swing.panel.LayerPaneShadow bg;
+    private com.huyhoang.employee.gui.component.Content content1;
+    private com.huyhoang.employee.gui.component.Header header1;
+    private com.huyhoang.employee.gui.component.Menu menu1;
     // End of variables declaration//GEN-END:variables
 }
